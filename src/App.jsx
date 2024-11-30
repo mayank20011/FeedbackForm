4cd4e9273c87f973c6552e01632f1589e139bf0e
mayank20011 <mayank.behl.mb1@gmail.com>
2024-11-30 17:33:37 +0530

    finalized form, just need to create alerts and connect to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import Feedback from "./Feedback";
-import Start from "./Start";
-import Confirmation from "./Confirmation";
-import { Route, Routes } from "react-router-dom";
 import {useState, useEffect} from "react";
 import { useLocation } from "react-router-dom";
 
@@ -24,12 +21,3 @@ function App(){
   </>)
 }
 export default App;
-
-
-
-
-{/* <Routes>
-   <Route path="/" element={<Start/>}/>
-   <Route path="/Feedback" element={<Feedback name={name} number={number}/>}/>
-   <Route path="/Confirmation" element={<Confirmation/>}/>
-   </Routes> */}
--- src/Feedback.jsx
+++ src/Feedback.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Star from "./assets/Starimages.png";
 import { useRef, useEffect, useState } from "react";
-
+import Confirmation from "./Confirmation.jsx";
 function Feedback({ name, number }) {
 
+  // To store what component to render
+  const [renderComponent, setComponent] = useState("Form");
+
   // to get input from textarea
-  const textarea=useRef(null);
+  const textarea = useRef(null);
 
   // to catch no of stars
   let noOfStars = 0;
@@ -13,20 +16,21 @@ function Feedback({ name, number }) {
   // function to handleSubmit
   function handleSubmit(e) {
     e.preventDefault();
-    const customerFeedBack={
-      "name":name,
-      "number":number,
-      "rating":noOfStars,
-      "feedBack":textarea.current.value,
-    }
+    const customerFeedBack = {
+      name: name,
+      number: number,
+      rating: noOfStars,
+      feedBack: textarea.current.value,
+    };
     if (noOfStars == 0) {
-      alert("Fill In the Form")
+      alert("Fill In the Form");
     } else {
       console.log(customerFeedBack);
+      setComponent("Confirmation");
       // axios.post("")
       // .then(()=>
       //   {
-            
+
       //   })
       //   .catch(()=>
       //   {
@@ -59,81 +63,83 @@ function Feedback({ name, number }) {
     });
   }
   return (
-    <form
-      className="flex flex-col min-h-screen gap-6 px-3 py-3 mx-auto"
-      style={{ maxWidth: "500px" }}
-      onSubmit={handleSubmit}
-    >
-      {/* For Heading */}
-      <h1 className="text-center text-4xl py-4 font-bold text-green-600">
-        Feedback Form
-      </h1>
-
-      {/* changes in name and phone number */}
-      <div className="grid gap-6">
-        {/* name div */}
-        <div className="border-2 border-black p-4 rounded-md flex items-center gap-3 relative">
-          <div
-            className="flex gap-3 items-center absolute bg-white text-green-600"
-            style={{ top: "-30%" }}
-          >
-            <i className="fa-solid fa-user"></i>
-            Customer Name
-          </div>
-          <p>{name}</p>
-        </div>
-        {/*  phone no div */}
-        <div className="border-2 border-black p-4 rounded-md flex items-center gap-3 relative">
-          <div
-            className="flex gap-3 items-center absolute bg-white text-green-600"
-            style={{ top: "-30%" }}
-          >
-            <i className="fa-solid fa-phone"></i>
-            Phone Number
-          </div>
-          <p>{`+${number}`}</p>
-        </div>
-      </div>
-
-      {/* For Rating */}
-      <div className="grid gap-2">
-        <h1 className="text-2xl font-bold">Rate Your Experience</h1>
-        <div className="flex gap-1">
-          {starRefs.current.map((ref, index) => (
-            <img
-              key={index}
-              src={Star}
-              alt="Star Image"
-              className="h-12 grayscale hover:scale-150 transition cursor-pointer"
-              ref={ref}
-              onClick={() => colorStars(index)}
-            />
-          ))}
-        </div>
-      </div>
-
-      {/* Product Feedback */}
-      <div className="grow flex flex-col gap-2">
-        <h1 className="text-2xl font-bold">Product Feedback</h1>
-        <textarea
-          name="Feedback"
-          id="Feedback"
-          placeholder="Share Your thoughts and experiences about the product... "
-          className="w-full h-48 outline-none border-2 p-2 resize-none rounded-md"
-          ref={textarea}
-        ></textarea>
-      </div>
-
-      {/* Button */}
-      <button
-        type="submit"
-        className="text-white font-normal w-full bg-green-600 rounded-md py-3 px-2 hover:scale-95 transition flex items-center justify-center gap-2 text-xl"
+    <div>
+      { renderComponent =="Confirmation" ? <Confirmation/> : <form
+        className="flex flex-col min-h-screen gap-6 px-3 py-3 mx-auto"
+        style={{ maxWidth: "500px" }}
+        onSubmit={handleSubmit}
       >
-        <i className="fa-regular fa-paper-plane"></i>
-        <p>Submit Feedback</p>
-      </button>
-    </form>
-  );
+        {/* For Heading */}
+        <h1 className="text-center text-4xl py-4 font-bold text-green-600">
+          Feedback Form
+        </h1>
+
+        {/* changes in name and phone number */}
+        <div className="grid gap-6">
+          {/* name div */}
+          <div className="border-2 border-black p-4 rounded-md flex items-center gap-3 relative">
+            <div
+              className="flex gap-3 items-center absolute bg-white text-green-600"
+              style={{ top: "-30%" }}
+            >
+              <i className="fa-solid fa-user"></i>
+              Customer Name
+            </div>
+            <p>{name}</p>
+          </div>
+          {/*  phone no div */}
+          <div className="border-2 border-black p-4 rounded-md flex items-center gap-3 relative">
+            <div
+              className="flex gap-3 items-center absolute bg-white text-green-600"
+              style={{ top: "-30%" }}
+            >
+              <i className="fa-solid fa-phone"></i>
+              Phone Number
+            </div>
+            <p>{`+${number}`}</p>
+          </div>
+        </div>
+
+        {/* For Rating */}
+        <div className="grid gap-2">
+          <h1 className="text-2xl font-bold">Rate Your Experience</h1>
+          <div className="flex gap-1">
+            {starRefs.current.map((ref, index) => (
+              <img
+                key={index}
+                src={Star}
+                alt="Star Image"
+                className="h-12 grayscale hover:scale-150 transition cursor-pointer"
+                ref={ref}
+                onClick={() => colorStars(index)}
+              />
+            ))}
+          </div>
+        </div>
+
+        {/* Product Feedback */}
+        <div className="grow flex flex-col gap-2">
+          <h1 className="text-2xl font-bold">Product Feedback</h1>
+          <textarea
+            name="Feedback"
+            id="Feedback"
+            placeholder="Share Your thoughts and experiences about the product... "
+            className="w-full h-48 outline-none border-2 p-2 resize-none rounded-md"
+            ref={textarea}
+          ></textarea>
+        </div>
+
+        {/* Button */}
+        <button
+          type="submit"
+          className="text-white font-normal w-full bg-green-600 rounded-md py-3 px-2 hover:scale-95 transition flex items-center justify-center gap-2 text-xl"
+        >
+          <i className="fa-regular fa-paper-plane"></i>
+          <p>Submit Feedback</p>
+        </button>
+      </form>}
+    </div>
+  )
 }
 
 export default Feedback;
