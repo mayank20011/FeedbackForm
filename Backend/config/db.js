9b5a8dc3554b7e21689d1bbe53bd9ee67d27e1c6
mayank20011 <mayank.behl.mb1@gmail.com>
2025-01-17 09:42:31 +0530

    added vercel.json

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -10,5 +10,10 @@ mongoose.connect(`${process.env.DBURI}`)
   })
 .catch((err)=>
   {
-    console.log("Connection to db Refused");
+    console.log(process.env.DBURI);
+    console.log(`Connection to db Refused ${err}`);
   })
+
+
+
+  // ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAuaW0Y+9F9YntXsYHsm6x3Z/mWWOnq1K2ezJGKmliw2H08HKscqfBf4G2ZQkkaepBWJbAyWMWwHILoIJUBTsxtYwZS1llW3bra+BS86yh6622DSAW/NNtxWI/dN9/FiOniNYXAL0LlzpVuFj0vRFMNTr8WA4rkgUCi+7bUZNi4UosTIDSg3Y4r+nqOsvJgHpp/m9dSdM8GJLkgQ8Npyck9U3FEVYrTYyk5jA2/V4QcoSzC03fOYvvzyhz4Zr8euMH32/lb96gX08NcPowEwhUWMGNh6hDiCQT9HfZnFeJ5ojVk/J8WOjFqoxaEzqSCExSdD0oTuxQBul2JSMz1u7VEQ== rsa-key-20241204
